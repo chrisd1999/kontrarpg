8197777d9f078bfe2ffeb7251ba25102b203cdaf
Kickster102 <80516040+Kickster102@users.noreply.github.com>
2021-03-13 18:52:13 +0200

    Scientist Movement and Animation + Bandit AIs
    Scientist can walk and run and also attack a Bandit.

---
+++ KontraRPG/Assets/Characters/Bandit/Bandit.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bandit : MonoBehaviour
+{
+    public int maxHealth=100;
+    int currentHealth;
+    public float lookRadius =25f;
+    public float attackRadius =7f;
+    public Vector3 walkPoint;
+    public bool walkPointSet;
+    public float walkPointRange;
+    
+    NavMeshAgent agent;
+    GameObject target;
+    CharacterController controller;
+    Animator anim;
+    
+    private void Start(){
+        currentHealth=maxHealth;
+        anim=GetComponent<Animator>();
+        agent=GetComponent<NavMeshAgent>();
+        target=GameObject.FindGameObjectWithTag("Player");
+    }
+    public void TakeDamage(int damage){
+        currentHealth-=damage;
+        anim.SetTrigger("Hurt");
+        if(currentHealth<=0){
+            Death();
+        }
+    }
+    void Death(){
+        anim.SetBool("isDead",true);
+        this.enabled=false;
+        GetComponent<Collider>().enabled=false;
+        GetComponent<NavMeshAgent>().enabled=false;
+    }
+    private void Update(){
+        float distance = Vector3.Distance(target.transform.position, transform.position);
+        float attackdistance = Vector3.Distance(target.transform.position, transform.position);
+        if(distance <=lookRadius && distance>attackRadius){
+             ChasePlayer();
+        }else if(distance <=lookRadius && attackdistance<=attackRadius){
+             AttackPlayer();
+        }else{
+             Patrolling();
+        }
+    }
+    private void ChasePlayer(){
+         //anim.SetBool("Draw_Sword",true);
+         anim.SetFloat("Bandit_Speed",0.5f,0.1f,Time.deltaTime);
+         agent.SetDestination(target.transform.position);
+    }
+    private void Patrolling(){
+       // anim.SetBool("Draw_Sword",false);
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            anim.SetFloat("Bandit_Speed",1f,0.1f,Time.deltaTime);
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f))
+            walkPointSet = true;
+    }
+    private void AttackPlayer()
+    {
+        anim.SetFloat("Bandit_Speed",0f,0.1f,Time.deltaTime);
+        anim.SetTrigger("Attack");
+        agent.SetDestination(transform.position);
+        transform.LookAt(target.transform);
+
+    }
+    
+}
---
+++ KontraRPG/Assets/Characters/Scientist/Player_Movement.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed=5;
+    [SerializeField] private float runSpeed=7;
+
+    public Transform AttackPoint;
+    public float attackRate=0.5f;
+    float nextAttackTime=0f;
+    public float attackRange=2f;
+    public int attackDamage=40;
+    public LayerMask enemyLayers;
+    private Vector3 moveDirection;
+    private Vector3 velocity;
+
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
+
+    private CharacterController controller;
+    private Animator anim;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+            Move();
+            if(Time.time >=nextAttackTime){
+                if(Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    Attack();
+                    nextAttackTime=Time.time+1f/attackRate;
+                }
+            }
+
+    }
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance,groundMask);
+        if(isGrounded && velocity.y<0){
+            velocity.y= -2f;
+        }
+        
+        float moveZ= Input.GetAxis("Vertical");
+
+        moveDirection=new Vector3(0,0,moveZ);
+        moveDirection=transform.TransformDirection(moveDirection);
+
+        if(isGrounded){
+            if(moveDirection!=Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+            {
+                Walk();
+            }
+             else if(moveDirection!=Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+            {
+                Run();
+
+            }else if(moveDirection == Vector3.zero){
+                Idle();
+
+            }
+            moveDirection *=moveSpeed;
+        }
+        
+        controller.Move(moveDirection * Time.deltaTime);
+        velocity.y +=gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+    private void Idle(){
+        anim.SetFloat("Speed",0f,0.1f,Time.deltaTime);
+    }
+    private void Walk(){
+        moveSpeed=walkSpeed;
+        anim.SetFloat("Speed",0.5f,0.1f,Time.deltaTime);
+    }
+    private void Run(){
+        moveSpeed=runSpeed;
+        anim.SetFloat("Speed",1f,0.1f,Time.deltaTime);
+    }
+    private void Attack(){
+        anim.SetTrigger("Attack");
+        Collider[] hitEnemies =Physics.OverlapSphere(AttackPoint.position,attackRange,enemyLayers);
+
+        foreach(Collider enemy in hitEnemies){
+            enemy.GetComponent<Bandit>().TakeDamage(attackDamage);
+        }
+    
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(AttackPoint.position,attackRange);
+    }
+}
