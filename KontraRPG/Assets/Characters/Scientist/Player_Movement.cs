a6e15ec03f24f7695b19464ddff1002af10cbe35
Kristers <45801982+chrisd1999@users.noreply.github.com>
2021-03-22 00:36:47 +0200

    Inventory Singleton
    Inventory Singleton

--- KontraRPG/Assets/Characters/Scientist/Player_Movement.cs
+++ KontraRPG/Assets/Characters/Scientist/Player_Movement.cs
@@ -10,14 +10,14 @@ public class Player_Movement : MonoBehaviour
 
 
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float walkSpeed=5;
-    [SerializeField] private float runSpeed=7;
+    [SerializeField] private float walkSpeed = 5;
+    [SerializeField] private float runSpeed = 7;
 
     [SerializeField] private Transform AttackPoint;
-    [SerializeField] private float attackRate=0.5f;
-    [SerializeField] private float nextAttackTime=0f;
-    [SerializeField] private float attackRange=2f;
-    [SerializeField] private int attackDamage=40;
+    [SerializeField] private float attackRate = 0.5f;
+    [SerializeField] private float nextAttackTime = 0f;
+    [SerializeField] private float attackRange = 2f;
+    [SerializeField] private int attackDamage = 40;
     [SerializeField] private LayerMask enemyLayers;
     [SerializeField] private Vector3 moveDirection;
     [SerializeField] private Vector3 velocity;
@@ -28,7 +28,9 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] private float gravity;
 
     [SerializeField] private CharacterController controller;
+
     [SerializeField] private Animator anim;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -37,95 +39,104 @@ public class Player_Movement : MonoBehaviour
 
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
-
     }
 
     // Update is called once per frame
     private void Update()
     {
-            Move();
-            if(Time.time >=nextAttackTime){
-                if(Input.GetKeyDown(KeyCode.Mouse0))
-                {
-                    Attack();
-                    nextAttackTime=Time.time + 1f / attackRate;
-                }
+        Move();
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
             }
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             TakeDamage(20);
         }
     }
+
     private void Move()
     {
-        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance,groundMask);
-        if(isGrounded && velocity.y<0){
-            velocity.y= -2f;
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
         }
-        
-        float moveZ= Input.GetAxis("Vertical");
 
-        moveDirection=new Vector3(0,0,moveZ);
-        moveDirection=transform.TransformDirection(moveDirection);
+        float moveZ = Input.GetAxis("Vertical");
 
-        if(isGrounded){
-            if(moveDirection!=Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+        moveDirection = new Vector3(0, 0, moveZ);
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        if (isGrounded)
+        {
+            if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
             {
                 Walk();
             }
-             else if(moveDirection!=Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+            else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
             {
                 Run();
-
-            }else if(moveDirection == Vector3.zero){
-                Idle();
-
             }
-            moveDirection *=moveSpeed;
+            else if (moveDirection == Vector3.zero)
+            {
+                Idle();
+            }
+
+            moveDirection *= moveSpeed;
         }
-        
+
         controller.Move(moveDirection * Time.deltaTime);
-        velocity.y +=gravity * Time.deltaTime;
+        velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
 
+    private void Idle()
+    {
+        anim.SetFloat("Speed", 0f, 0.1f, Time.deltaTime);
     }
-    private void Idle(){
-        anim.SetFloat("Speed",0f,0.1f,Time.deltaTime);
+
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+        anim.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
     }
-    private void Walk(){
-        moveSpeed=walkSpeed;
-        anim.SetFloat("Speed",0.5f,0.1f,Time.deltaTime);
+
+    private void Run()
+    {
+        moveSpeed = runSpeed;
+        anim.SetFloat("Speed", 1f, 0.1f, Time.deltaTime);
     }
-    private void Run(){
-        moveSpeed=runSpeed;
-        anim.SetFloat("Speed",1f,0.1f,Time.deltaTime);
-    }
-    private void Attack(){
+
+    private void Attack()
+    {
         anim.SetTrigger("Attack");
-        Collider[] hitEnemies =Physics.OverlapSphere(AttackPoint.position,attackRange,enemyLayers);
+        Collider[] hitEnemies = Physics.OverlapSphere(AttackPoint.position, attackRange, enemyLayers);
 
-        foreach(Collider enemy in hitEnemies){
+        foreach (Collider enemy in hitEnemies)
+        {
             enemy.GetComponent<Bandit>().TakeDamage(attackDamage);
         }
-    
     }
+
     void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(AttackPoint.position,attackRange);
+        Gizmos.DrawWireSphere(AttackPoint.position, attackRange);
     }
 
     //health bar
 
-    
-   
-
-    
 
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
     }
-    
+
     //healtbar end
 }
---
+++ KontraRPG/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Inventory
+{
+    public class Inventory : MonoBehaviour
+    {
+        [SerializeField] private int _space;
+        public static Inventory Instance;
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                print("More than one Inventory instance.");
+                return;
+            }
+            Instance = this;
+        }
+    }
+}
