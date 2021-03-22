3140be8363f065580e2ade251de3a73a49fea7c1
Kristers <45801982+chrisd1999@users.noreply.github.com>
2021-03-22 19:39:46 +0200

    Added Pause Menu
    Added Pause Menu

---
+++ KontraRPG/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    private static bool _gameIsPaused = false;
+    void Update()
+    {
+        if (Input.GetButtonDown("PauseMenu"))
+        {
+            ShowPauseMenu();
+        }
+    }
+
+    private void ShowPauseMenu()
+    {
+        if (_gameIsPaused)
+        {
+            Resume();
+            return;
+        }
+
+        Pause();
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        _gameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        _gameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
