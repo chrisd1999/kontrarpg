9ddf4650d2b6186db05c2d32e0a88ae9c67ca3f5
Kristers <45801982+chrisd1999@users.noreply.github.com>
2021-04-15 23:30:09 +0300

    Implemented Spawner for Enemies
    Implemented Spawner for Enemies

---
+++ KontraRPG/Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _monster;
+    [SerializeField] private float _spawnDelay;
+    private float _currentTime;
+    private bool _respawn;
+    private bool _spawning;
+
+    private void Start()
+    {
+        Spawn();
+        _currentTime = _spawnDelay;
+    }
+
+    private void Update()
+    {
+        if (!_spawning) return;
+
+        _currentTime -= _spawnDelay;
+        if (_currentTime <= 0)
+        {
+            Spawn();
+        }
+    }
+
+    public void Respawn()
+    {
+        _spawning = true;
+        _currentTime = _spawnDelay;
+    }
+    
+    private void Spawn()
+    {
+        IEnemy instance = Instantiate(_monster, transform.position, Quaternion.identity).GetComponent<IEnemy>();
+        instance.Spawner = this;
+        
+        _spawning = false;
+    }
+}
