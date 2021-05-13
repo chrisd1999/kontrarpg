9c51eb748c7906e9d521bd98e724223c95faf9e5
Kristers <45801982+chrisd1999@users.noreply.github.com>
2021-05-14 00:20:03 +0300

    Added Map menu toggle
    Map menu with button M.

---
+++ KontraRPG/Assets/Scripts/MapMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject map;
+    private bool _mapEnabled = false;
+    void Update()
+    {
+        if (Input.GetButtonDown("Map"))
+        {
+            _mapEnabled = !_mapEnabled;
+        }
+        ShowMap();
+    }
+
+    private void ShowMap()
+    {
+        if (_mapEnabled == true)
+        {
+            map.SetActive(true);
+            return;
+        }
+        map.SetActive(false);
+    }
+}
