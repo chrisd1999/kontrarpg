ba42af39809c2ee274df7ddbc971a2294689f8c5
Kristers <45801982+chrisd1999@users.noreply.github.com>
2021-05-17 23:20:22 +0300

    Created new item WitchKey with key sprite

---
+++ KontraRPG/Assets/Scripts/Inventory/Items/WitchKey.cs
@@ -0,0 +1,25 @@
+﻿using Interfaces;
+using UnityEngine;
+
+namespace Inventory.Items
+{
+    public class WitchKey : IItem
+    {
+        private string _name = "Witch Key";
+        private Sprite _icon;
+
+        public WitchKey()
+        {
+            _icon = Resources.Load<Sprite>("Sprites/key");
+        }
+        public string Name
+        {
+            get => _name;
+        }
+
+        public Sprite Icon
+        {
+            get => _icon;
+        }
+    }
+}
